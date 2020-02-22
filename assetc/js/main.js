b01de9d3befb7f876b81baf4d83b3b08adcd5b61
Your Name <mollahsamim4@gmail.com>
2020-02-22 20:15:31 +0530

    First Commit

---
+++ assetc/js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener("keydown", function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key=document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) return //if audio element not found than stop code
+    audio.currentTime = 0 //assing audio currentTime property intp zero
+    console.log(audio.currentTime);
+    audio.play();
+   key.classList.add("playing");
+});
+
+const keys=document.querySelectorAll(".key");
+keys.forEach(function(key){
+    key.addEventListener("transitionend",function(e){
+        if(e.propertyName!=="transform") return//If not Found Transform Propery
+        this.classList.remove("playing");
+    })
+})
