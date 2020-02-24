d3f20a4f32a4c35b147d7ad9b4c98225bf08cb3e
Your Name <mollahsamim4@gmail.com>
2020-02-24 22:36:02 +0530

    javscript css variable

--- assetc/js/main.js
+++ assetc/js/main.js
@@ -1,26 +1,7 @@
-const hour_hand=document.querySelector(".hour_hand");
-const min_hand=document.querySelector(".min_hand");
-const second_hand=document.querySelector(".second_hand");
-
-function setDate(){
-	const date=new Date();
-	//For Seconds Hand
-	const seconds=date.getSeconds();
-	const secondDegree=((seconds/60)*360)+90;
-	second_hand.style.transform=`rotate(${secondDegree}deg)`;
-
-	//For Minute Hand
-	const minutes=date.getMinutes();
-	const minuteDegree=((minutes/60)*360)+90;
-	min_hand.style.transform=`rotate(${minuteDegree}deg)`;
-
-	//For Hour Hand
-	const hour=date.getMinutes();
-	const hourdegree=((minutes / 12) * 360 ) + 90;
-	let hourHands=hour_hand.style.transform=`rotate(${hourdegree}deg)`;
-
-
-	
+const inputs=document.querySelectorAll(`#controls input`);
+function changeHandler(){
+	const suffix=this.dataset.sizing || '';
+	document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
 }
-
-setInterval(setDate,1000);
+inputs.forEach(input=>input.addEventListener("change",changeHandler));
+inputs.forEach(input=>input.addEventListener("mousemove",changeHandler));
