51e998a688d57bbc710d7dd6e1296a0e1ae590f4
Your Name <mollahsamim4@gmail.com>
2020-02-25 13:20:16 +0530

    Something Is Changed..

--- assetc/js/main.js
+++ assetc/js/main.js
@@ -1,7 +1,12 @@
-const inputs=document.querySelectorAll(`#controls input`);
-function changeHandler(){
-	const suffix=this.dataset.sizing || '';
-	document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
+const inputs=document.querySelectorAll(`.controls .control_area input`);
+const selectArea=document.querySelectorAll(`.controls select`);
+
+function inputHandle(){
+ const suffix=this.dataset.sizing || '';
+document.documentElement.style.setProperty(`--${this.name}`,this.value+suffix);
 }
-inputs.forEach(input=>input.addEventListener("change",changeHandler));
-inputs.forEach(input=>input.addEventListener("mousemove",changeHandler));
+
+inputs.forEach(input=>input.addEventListener("change",inputHandle));
+
+//For Select Column
+selectArea.forEach(input=>input.addEventListener("change",inputHandle));
