a24600ff64b3c734fae766ba602f33e7530d8cd5
Your Name <mollahsamim4@gmail.com>
2020-02-28 10:56:17 +0530

    Select Check Box In Between check list

--- assetc/js/main.js
+++ assetc/js/main.js
@@ -1,46 +1,23 @@
-const canvas=document.querySelector("#draw");
-const ctx=canvas.getContext("2d");
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-ctx.strokeStyle="#BADA55";
-ctx.lineJoin="round";
-ctx.lineCap="round";
-ctx.lineWidth=100;
-let isDrawing=false;
-let lastX=0;
-let lastY=0;
-let hue=0;
-let direction=true;
-function draw(e){
-	if(!isDrawing) return //stop Code if isDrawing is false;
-	ctx.strokeStyle=`hsl(${hue},100%,50%)`;
-	console.log(e);
-	ctx.beginPath();
-	ctx.moveTo(lastX,lastY);
-	ctx.lineTo(e.offsetX,e.offsetY);
-	ctx.stroke();
-	[lastX,lastY]=[e.offsetX,e.offsetY];
-	hue++;
-	if(hue>360){
-		hue=0;
+
+
+const checkbox=document.querySelectorAll(".inbox .item input[type='checkbox']");
+let lastChecked;
+let inBetween=false;
+
+function handleCheck(e){
+	if(e.ctrlKey && this.checked){
+		checkbox.forEach(checkedItem=>{
+			if(checkedItem==this || checkedItem==lastChecked){
+				inBetween=!inBetween;
+			}
+			if(inBetween){
+				checkedItem.checked=true;
+			}
+		})
 	}
-	if(ctx.lineWidth>=200 || ctx.lineWidth<=1){
-		direction=!direction;
-	}
-	if(direction){
-		ctx.lineWidth++;
-	}
-	else{
-		ctx.lineWidth--;
-	}
+	lastChecked=this;
 }
 
 
-canvas.addEventListener("mousedown",(e)=>{
-	isDrawing=true;
-	[lastX,lastY]=[e.offsetX,e.offsetY];
-});
-canvas.addEventListener("mousemove",draw);
+checkbox.forEach(checItem=>checItem.addEventListener("click",handleCheck));
 
-canvas.addEventListener("mouseup",()=>isDrawing=false);
-canvas.addEventListener("mouseout",()=>isDrawing=false);
